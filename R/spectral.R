6b310f59718eeace892f491c0e806dd069e84f8e
JadenWSR <jadenwangsr@gmail.com>
2022-11-17 14:03:11 -0500

    Improved test convergence

--- R/spectral.R
+++ R/spectral.R
@@ -1,12 +1,10 @@
-
-
 #' Get the Spectral Signature of Accelerometry Data
 #'
 #' The spectral signature is calculated by taking the modulus of the
 #' Fourier coefficients of the signal.
 #' @param x an object inherited from a data.frame with columns X, Y, Z, and time
 #' sorted in time.
-#' @param take_log should the log of the modulus be taken. (Default is `TRUE`)
+#' @param take_log should the log of the modulus be taken. (Default is `FALSE`)
 #' @param inverse should the unnormalized inverse transform is computed.
 #' (Default is `TRUE`)
 #' @return a data frame with the modulus of the Fourier coefficients for the
